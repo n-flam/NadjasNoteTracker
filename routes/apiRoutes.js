968d83133750a68426acf6b6dca805ffe75dca43
nflam <nadja.flam@gmx.net>
2021-04-20 13:06:01 +0800

    get notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,13 @@ const fs = require("fs")
 const path = require('path');
 const { raw } = require('express');
 
+//get notes
+router.get('/notes', (req, res) => {
+  let rawdata = fs.readFileSync(DB_PATH, 'utf-8');
+  let db = JSON.parse(rawdata)
+  res.json(db);
+  }
+);
 
 
 module.exports = router;
